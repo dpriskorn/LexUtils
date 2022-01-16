8d92c1a1080ebc56e119cdc5d58eee86e5762c0a
Dennis Priskorn <68460690+dpriskorn@users.noreply.github.com>
2022-01-16 23:02:17 +0100

    Detect lowercase and weird characters and if swedish before adding.

--- convert_riksdagen_txt_to_pandas.py
+++ convert_riksdagen_txt_to_pandas.py
@@ -3,10 +3,10 @@ import re
 import time
 import zipfile as ziplib
 
+import langdetect
 import pandas as pd
 from spacy.lang.sv import Swedish
 
-from lexutils.helpers.console import console
 pickle_filename = "departementsserien.pkl.gz"
 dir = r"riksdagen/ds-text/"
 files = os.listdir(dir)
@@ -22,7 +22,7 @@ for zipfile in files:
         with ziplib.ZipFile(dir + zipfile) as z:
             count_file = 1
             for filename in z.namelist():
-               #print(filename)
+                # print(filename)
                 document_id = filename.replace(".txt", "")
                 print(f"working on {document_id} ({count_file}/{len(z.namelist())}) "
                       f"skipped: {skipped_count} dataframe rows: {len(df)} splits: {split_count}")
@@ -56,26 +56,42 @@ for zipfile in files:
                             text_after_split = text.split("1")
                             print(f"len(text_after_split):{len(text_after_split)}")
                             split_count += 1
-                            #exit(0)
+                            # exit(0)
                         sentences = set()
                         for text in text_after_split:
                             doc = nlp(text)
                             for sentence in doc.sents:
+                                sentence = str(sentence)
                                 # Skip all sentences with numbers
                                 # https://stackoverflow.com/questions/4289331/how-to-extract-numbers-from-a-string-in-python
-                                if len(re.findall(r'\b\d+\b', str(sentence))) == 0:
-                                    sentences.add(sentence)
+                                if (
+                                        len(sentence.split(" ")) > 4 and
+                                        # Remove sentences with digits and (, ), [, ], §, /
+                                        len(re.findall(r'\d+|\(|\)|§|\[|\]|\/', sentence)) == 0 and
+                                        sentence[0:1] != " " and
+                                        sentence[0:1] != "," and
+                                        not sentence[0:1].islower() and
+                                        langdetect.detect(sentence) == "sv"
+                                ):
+                                    # Ta bort punktum från början
+                                    sentences.add(sentence.lstrip("."))
                                 else:
                                     skipped_count += 1
                                     # console.print(f"skipped: {sentence}", style="red")
                         for sentence in sentences:
-                            df = df.append(pd.DataFrame(data=[dict(id=document_id,sentence=sentence)]))
+                            # print(type(sentence))
+                            dictionary = dict(id=document_id, sentence=sentence)
+                            print(dictionary)
+                            # exit()
+                            df = df.append(pd.DataFrame(data=[dictionary]))
                             # print(sentence)
                             # print("--")
                 count_file += 1
-                #break
-    #break
-print(df.info(), df.describe())
+                if count_file == 2:
+                    break
+            # break
+    # break
+print(df.info(), df.describe(), df.sample(10))
 df.to_pickle(pickle_filename)
 end = time.time()
 print(f"total duration: {round(end - start)}s")
