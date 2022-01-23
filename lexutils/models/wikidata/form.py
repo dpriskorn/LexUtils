87c4741d2d1828311f697966a6af1ad8ecca00c5
Dennis Priskorn <68460690+dpriskorn@users.noreply.github.com>
2022-01-23 21:04:46 +0100

    Improve Form form.py: 3 new methods: hangor_url(), orthohin_url() and  wikidata_search_url() fetch_senses(): Switch to console status. __init__(): Change signature json->entry_data

--- lexutils/models/wikidata/form.py
+++ lexutils/models/wikidata/form.py
@@ -1,16 +1,18 @@
 import logging
 from typing import List, Optional
+from urllib.parse import quote
 
 from wikibaseintegrator import WikibaseIntegrator
 from wikibaseintegrator.models import LanguageValue
 from wikibaseintegrator.wbi_helpers import execute_sparql_query
 
-from lexutils.config import config
+from lexutils.config import config, constants
 from lexutils.config.config import login_instance
-from lexutils.helpers import tui
 from lexutils.helpers.caching import read_from_cache, add_to_cache
+from lexutils.helpers.console import console
 from lexutils.models.usage_example import UsageExample
 from lexutils.models.wikidata.entities import EntityID
+from lexutils.models.wikidata.enums import WikimediaLanguageCode
 from lexutils.models.wikidata.sense import Sense
 
 
@@ -21,6 +23,7 @@ class Form:
     id: str
     representation: str
     grammatical_features: List[str]
+    language_code: WikimediaLanguageCode = None
     # We store these on the form because they are needed
     # to determine if an example fits or not
     lexeme_id: str
@@ -29,25 +32,33 @@ class Form:
     senses: List[Sense] = None
     usage_examples: List[UsageExample]
 
-    def __init__(self, json):
-        """Parse the form json"""
+    def __init__(
+            self,
+            entry_data,
+            language_code: WikimediaLanguageCode = None
+    ):
+        """Parse the form entry_data"""
+        # TODO split this up into methods
         logger = logging.getLogger(__name__)
+        if language_code is None:
+            raise ValueError("language_code was None")
+        self.language_code = language_code
         try:
-            logger.debug(json["lexeme"])
-            self.lexeme_id = str(EntityID(json["lexeme"]["value"]))
+            logger.debug(entry_data["lexeme"])
+            self.lexeme_id = str(EntityID(entry_data["lexeme"]["value"]))
         except KeyError:
             pass
         try:
-            logger.debug(json["form"])
-            self.id = str(EntityID(json["form"]["value"]))
+            logger.debug(entry_data["form"])
+            self.id = str(EntityID(entry_data["form"]["value"]))
         except KeyError:
             pass
         try:
-            self.representation: str = json["form_representation"]["value"]
+            self.representation: str = entry_data["form_representation"]["value"]
         except KeyError:
             pass
         try:
-            qid = str(EntityID(json["category"]["value"]))
+            qid = str(EntityID(entry_data["category"]["value"]))
             label: Optional[str] = read_from_cache(qid=qid)
             logger.debug(f"got {label} from the cache")
             if label is not None:
@@ -66,11 +77,13 @@ class Form:
             # exit()
         except ValueError:
             logger.error(f'Could not find lexical category from '
-                         f'{json["category"]["value"]}')
+                         f'{entry_data["category"]["value"]}')
+        except KeyError:
+            pass
         try:
             self.grammatical_features = []
-            logger.debug(json["grammatical_features"])
-            for feature in json["grammatical_features"]["value"].split(","):
+            logger.debug(entry_data["grammatical_features"])
+            for feature in entry_data["grammatical_features"]["value"].split(","):
                 qid = str(EntityID(feature))
                 label: Optional[str] = read_from_cache(qid=qid)
                 logger.debug(f"got {label} from the cache")
@@ -105,7 +118,7 @@ class Form:
         """Fetch the senses on the lexeme"""
 
         def sparql_query(fallback: bool = False):
-            if fallback == True:
+            if fallback:
                 # Fall back to English as gloss language
                 return execute_sparql_query(
                     f'''
@@ -157,28 +170,43 @@ class Form:
             raise ValueError("usage_example was None")
         # Thanks to Lucas Werkmeister https://www.wikidata.org/wiki/Q57387675 for
         # helping with this query.
-        tui.fetching_senses()
-        logging.info(f"...from {self.lexeme_id}")
-        result = sparql_query()
-        self.senses = []
-        # TODO Move this into the model
-        if result is not None:
-            extract_and_convert_the_result_to_senses(result)
-            if len(self.senses) == 0:
-                logger.warning(f"No senses with a gloss in the  "
-                               f"{usage_example.record.language_code.name.title()} was found. "
-                               f"Please go to {self.url()} and improve the glosses if you can. "
-                               f"Falling back to English as gloss language.")
-                result = sparql_query(fallback=True)
-                if result is not None:
-                    extract_and_convert_the_result_to_senses(result)
-                    if len(self.senses) == 0:
-                        logger.warning(f"No senses with a gloss in the fallback language English "
-                                       f" was found. "
-                                       f"Please go to {self.url()} and improve the glosses if you can.")
-        else:
-            raise ValueError("Error. Got None trying to fetch senses. " +
-                             "Please report this as an issue.")
+        with console.status("Fetching senses..."):
+            logging.info(f"...from {self.lexeme_id}")
+            result = sparql_query()
+            self.senses = []
+            # TODO Move this into the model
+            if result is not None:
+                extract_and_convert_the_result_to_senses(result)
+                if len(self.senses) == 0:
+                    logger.warning(f"No senses with a gloss in the  "
+                                   f"{usage_example.record.language_code.name.title()} was found. "
+                                   f"Please go to {self.url()} and improve the glosses if you can. "
+                                   f"Falling back to English as gloss language.")
+                    result = sparql_query(fallback=True)
+                    if result is not None:
+                        extract_and_convert_the_result_to_senses(result)
+                        if len(self.senses) == 0:
+                            logger.warning(f"No senses with a gloss in the fallback language English "
+                                           f" was found. "
+                                           f"Please go to {self.url()} and improve the glosses if you can.")
+            else:
+                raise ValueError("Error. Got None trying to fetch senses. " +
+                                 "Please report this as an issue.")
 
     def url(self):
         return f"{config.wd_prefix}{self.id}"
+
+    def hangor_url(self):
+        return f"{constants.hangor}lex/{self.language_code.value}/{self.representation}"
+
+    def orthohin_url(self):
+        return f"{constants.orthohin}add/{self.language_code.value}"
+
+    def wikidata_search_url(self):
+        # quote to guard agains äöå and the like
+        return (
+                "https://www.wikidata.org/w/index.php?" +
+                "search={}&title=Special%3ASearch&".format(quote(self.representation)) +
+                "profile=advanced&fulltext=0&" +
+                "advancedSearch-current=%7B%7D&ns0=1"
+        )
