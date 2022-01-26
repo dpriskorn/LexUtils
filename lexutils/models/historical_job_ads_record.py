20e77c02042fbb08448062141b56a68c452cc0c3
Dennis Priskorn <68460690+dpriskorn@users.noreply.github.com>
2022-01-26 10:30:08 +0100

    Remove dataclass which did not work. record.py: New attribute filename. Uncomment init and add new signatures: filename, date. extract_usage_example_if_suitable(): New method pulled up from riksdagen_record.py

--- lexutils/models/historical_job_ads_record.py
+++ lexutils/models/historical_job_ads_record.py
@@ -1,20 +1,10 @@
 from __future__ import annotations
-import json
+
 import logging
-from datetime import datetime
-from typing import Any, List, TYPE_CHECKING
 
-from spacy.lang.sv import Swedish
-
-from lexutils.config import config
 from lexutils.config.enums import LanguageStyle, ReferenceType, SupportedExampleSources, BaseURLs
 from lexutils.models.record import Record
-from lexutils.models.usage_example import UsageExample
 from lexutils.models.wikidata.enums import WikimediaLanguageCode
-from lexutils.models.wikidata.form import Form
-
-if TYPE_CHECKING:
-    pass
 
 logger = logging.getLogger(__name__)
 
@@ -24,75 +14,9 @@ class HistoricalJobAd(Record):
     language_style = LanguageStyle.FORMAL
     type_of_reference = ReferenceType.WRITTEN
     source = SupportedExampleSources.HISTORICAL_ADS
-    # TODO is not present in the dataset unfortunately
+    # TODO is not present in the dataset unfortunately but we check during preprocessing
+    # so the number of english sentences should be very few
     language_code = WikimediaLanguageCode.SWEDISH
-
-    def __init__(self, json_data: Any = None):
-        if json_data is None:
-            raise ValueError("json_data was None")
-        else:
-            # logger.info("Parsing ad")
-            data = json.loads(json_data)
-            self.id = data["id"]
-            self.date = datetime.strptime(data["publication_date"], "%Y-%m-%dT%H:%M:%S")
-            description = data["description"]
-            if "text" in description:
-                self.text = description["text"]
-                # TODO detect language
-                # logger.info("Detecting the language")
-                # self.language_code = WikimediaLanguageCode(langdetect.detect(self.text))
-                # logger.info(f"Detected: {self.language_code.name.title()}")
-            else:
-                logger.warning("found no text in this ad")
-
-    # TODO improve preprocessing of the dataframe so that we
-    #  already have sentences like for the riksdagen data
-    def find_usage_examples_from_summary(
-            self,
-            form: Form = None,
-    ) -> List[UsageExample]:
-        """This tries to find and clean sentences and return the shortest one"""
-        if form is None:
-            raise ValueError("form was None")
-        logger = logging.getLogger(__name__)
-        # find sentences
-        # order in a list by length
-        # pick the shortest one where the form representation appears
-        logger.debug("Splitting the sentences using spaCy")
-        nlp = Swedish()
-        nlp.add_pipe('sentencizer')
-        doc = nlp(self.text)
-        sentences = set()
-        raw_sentences = list(doc.sents)
-        logger.info(f"Got {len(raw_sentences)} sentences from spaCy")
-        for sentence in raw_sentences:
-            # logger.info(sentence.text)
-            # This is a very crude test for relevancy, we lower first to improve matching
-            cleaned_sentence = sentence.text.lower()
-            punctations = [".", ",", "!", "?", "„", "“", "\n"]
-            for punctation in punctations:
-                if punctation in cleaned_sentence:
-                    cleaned_sentence = cleaned_sentence.replace(punctation, " ")
-            logger.debug(f"cleaned sentence:{cleaned_sentence}")
-            if f" {form.representation.lower()} " in f" {cleaned_sentence} ":
-                # Add to the set first to avoid duplicates
-                sentences.add(sentence.text.replace("\n", "").strip())
-        logger.info(f"Found {len(sentences)} sentences which contained {form.representation}")
-        examples = []
-        count_discarded = 0
-        for sentence in sentences:
-            sentence_length = len(sentence.split(" "))
-            if (
-                    config.min_word_count < sentence_length < config.max_word_count
-            ):
-                examples.append(UsageExample(text=sentence, record=self))
-            else:
-                count_discarded += 1
-        if count_discarded > 0:
-            logger.info(f"{count_discarded} sentence was discarded based on length")
-        # print("debug exit")
-        # exit(0)
-        return examples
 
     def url(self):
         """This shadows the function in Record
