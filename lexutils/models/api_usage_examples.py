589886fc9cddb7a107dc3d56a2ee19c7aa1b8fc0
Dennis Priskorn <egils@riseup.net>
2023-01-30 07:25:48 +0100

    chore: Disable api_usage_examples.py 0 added 1 modified 0 deleted

--- lexutils/models/api_usage_examples.py
+++ lexutils/models/api_usage_examples.py
@@ -1,33 +1,29 @@
-from __future__ import annotations
-from abc import ABC, abstractmethod
-from typing import TYPE_CHECKING
-
-from lexutils.models.usage_examples import UsageExamples
-
-if TYPE_CHECKING:
-    from lexutils.models.lexemes import Lexemes
-    from lexutils.models.wikidata.form import Form
-
-
-class APIUsageExamples(UsageExamples, ABC):
-    def __init__(
-            self,
-            form: Form = None,
-            lexemes: Lexemes = None
-    ):
-        if form is None:
-            raise ValueError("form was None")
-        if lexemes is None:
-            raise ValueError("lexemes was None")
-        self.form = form
-        self.lexemes = lexemes
-        self.get_records()
-        self.process_records_into_usage_examples()
-
-    @abstractmethod
-    def get_records(self):
-        pass
-
-    @abstractmethod
-    def process_records_into_usage_examples(self):
-        pass
+# from __future__ import annotations
+#
+# from abc import abstractmethod
+# from typing import TYPE_CHECKING
+#
+# from lexutils.exceptions import MissingInformationError
+# from lexutils.models.usage_examples import UsageExamples
+#
+# if TYPE_CHECKING:
+#     from lexutils.models.lexemes import Lexemes
+#     from lexutils.models.wikidata.lexutils_form import LexutilsForm
+#
+#
+# class APIUsageExamples(UsageExamples):
+#     def get_and_process_records(self):
+#         if self.form is None:
+#             raise MissingInformationError("form was None")
+#         if self.lexemes is None:
+#             raise MissingInformationError("lexemes was None")
+#         self.get_records()
+#         self.process_records_into_usage_examples()
+#
+#     @abstractmethod
+#     def get_records(self):
+#         pass
+#
+#     @abstractmethod
+#     def process_records_into_usage_examples(self):
+#         pass
