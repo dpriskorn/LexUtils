5c669aa7f36af144ec9bf816d5cca93cb0c11eae
Dennis Priskorn <egils@riseup.net>
2023-01-30 08:47:03 +0100

    feat: Simplify Record and test it 1 added 1 modified 0 deleted

--- lexutils/models/record.py
+++ lexutils/models/record.py
@@ -1,93 +1,107 @@
 import logging
-from dataclasses import dataclass
 from datetime import datetime
-from typing import List, Optional
+from typing import Any, Optional
 from urllib.parse import quote
 
-from lexutils.config import config
+from pydantic import BaseModel
+
+import config
+from lexutils.enums import LanguageStyle, ReferenceType, SupportedExampleSources
 from lexutils.models.usage_example import UsageExample
 from lexutils.models.wikidata.enums import WikimediaLanguageCode
-from lexutils.models.wikidata.form import Form
+
+logger = logging.getLogger(__name__)
 
 
-class Record:
-    base_url: str = None
-    id: str = None
-    date: datetime = None
+class Record(BaseModel):
+    base_url: str = ""
+    id: str = ""
+    date: Optional[datetime] = None
     # Used for Riksdagen records
-    document_qid = None
+    document_qid: str = ""
     exact_hit: bool = False
     inexact_hit: bool = False
-    language_code: WikimediaLanguageCode = None
-    language_style = LanguageStyle = None
+    language_code: WikimediaLanguageCode
+    language_style: LanguageStyle
     # This is needed for the europarl source
-    line_number: int = None
-    source = SupportedExampleSources = None
-    text: str = None
-    type_of_reference = ReferenceType = None
-    filename: str = None
+    line_number: int = 0
+    source: SupportedExampleSources
+    text: str = ""
+    type_of_reference: ReferenceType
+    filename: str = ""
+    cleaned_sentence: str = ""
+    form: Any = None
 
-    def __init__(
-            self,
-            id: str = None,
-            text: str = None,
-            date: datetime = None,
-            filename: str = None
-    ):
-        if id is None:
-            raise ValueError("id was None")
-        if text is None:
-            raise ValueError("text was None")
-        self.text = text
-        self.id = id
-        # Optional
-        if date is not None:
-            self.date = date
-        if filename is not None:
-            self.filename = filename
-
-    def find_usage_examples_from_summary(
-            self,
-            form: Form = None,
-    ) -> List[UsageExample]:
-        pass
-
-    def extract_usage_example_if_suitable(
-            self,
-            form: Form = None,
+    #    @validate_arguments
+    def get_usage_example_if_representation_could_be_found(
+        self,
     ) -> Optional[UsageExample]:
-        # check if the form appears by first cleaning
-        # away all chars and then split the text into words
-        if form is None:
-            raise ValueError("form was None")
-        logger = logging.getLogger(__name__)
+        logger.debug("extract_usage_example_if_suitable: running")
+        from lexutils.models.wikidata.lexutils_form import LexutilsForm
+
+        print(self.form)
+        if not isinstance(self.form, LexutilsForm):
+            raise ValueError("form not a LexutilsForm")
+        self.__clean_sentence__()
+        return self.__find_representation_and_extract_usage_example__()
+
+    # def lookup_qid(self):
+    #     pass
+
+    @property
+    def url(self):
+        if not self.base_url:
+            raise ValueError(f"base_url was None for {self.source.name.title()}")
+        return f"{self.base_url}{quote(self.id)}"
+
+    # @property
+    # def human_readable_url(self):
+    #     return f"{self.base_url}{quote(self.id)}"
+
+    def __clean_sentence__(self):
+        logger.debug("__clean_sentence__: running")
         # This is a very crude test for relevancy, we lower first to improve matching
         logger.debug(f"Sentence before cleaning: {self.text}")
         cleaned_sentence = self.text.lower()
-        punctuations = [".", ",", "!", "?", "„", "“", "\n",
-                       ":", ";", "`", "´", "$", "€"]
+        punctuations = [
+            ".",
+            ",",
+            "!",
+            "?",
+            "„",
+            "“",
+            "\n",
+            ":",
+            ";",
+            "`",
+            "´",
+            "$",
+            "€",
+        ]
         for punctuation in punctuations:
             if punctuation in cleaned_sentence:
                 cleaned_sentence = cleaned_sentence.replace(punctuation, " ")
         logger.debug(f"cleaned sentence: {cleaned_sentence}")
-        if form.representation.lower() in cleaned_sentence.split():
-            logger.info(f"The form '{form.representation}' was found in the cleaned sentence. :)")
-            sentence_length = len(cleaned_sentence.split(" "))
-            if (
-                    config.min_word_count < sentence_length < config.max_word_count
-            ):
-                return UsageExample(text=self.text,
-                                    record=self)
+        self.cleaned_sentence = cleaned_sentence
+
+    def __find_representation_and_extract_usage_example__(
+        self,
+    ):
+        logger.debug("__find_representation_and_extract_usage_example__: running")
+        # split on any whitespace
+        words = self.cleaned_sentence.split()
+        if self.form.localized_representation.lower() in words:
+            logger.info(
+                f"The form '{self.form.localized_representation}' was found in the cleaned sentence. :)"
+            )
+            number_of_words = len(words)
+            if config.min_word_count < number_of_words < config.max_word_count:
+                return UsageExample(text=self.text, record=self)
             else:
-                logger.debug(f"{cleaned_sentence} was discarded based on length")
-
-    def lookup_qid(self):
-        pass
-
-    def url(self):
-        if self.base_url is None:
-            raise ValueError(f"base_url was None for {self.source.name.title()}")
-        return f"{self.base_url}{quote(self.id)}"
-
-    def human_readable_url(self):
-        return self.url()
+                logger.debug(f"{self.cleaned_sentence} was discarded based on length")
+                return None
+        else:
+            logger.debug(
+                f"could not find localized representation in {self.cleaned_sentence}"
+            )
+            return None
---
+++ tests/test_record.py
@@ -0,0 +1,99 @@
+from unittest import TestCase
+
+from lexutils.enums import LanguageStyle, SupportedExampleSources, ReferenceType
+from lexutils.models.record import Record
+from lexutils.models.usage_example import UsageExample
+from lexutils.models.wikidata.enums import WikimediaLanguageCode
+from lexutils.models.wikidata.lexutils_form import LexutilsForm
+
+
+class TestRecord(TestCase):
+    example_form: LexutilsForm = None
+
+    def setUp(self) -> None:
+        self.__setup_example_form__()
+
+    def __setup_example_form__(self):
+        # skrutits
+        form = LexutilsForm(form_id="L38817-F9")
+        form.language_code = WikimediaLanguageCode.SWEDISH
+        form.setup_lexeme()
+        self.example_form = form
+
+    def test_get_usage_example_if_representation_could_be_found_too_short(self):
+        record = Record(
+            id="1",
+            text=" skrutits ",
+            language_code=WikimediaLanguageCode.SWEDISH,
+            language_style=LanguageStyle.INFORMAL,
+            source=SupportedExampleSources.HISTORICAL_ADS,
+            type_of_reference=ReferenceType.WRITTEN,
+            form=self.example_form,
+        )
+        assert record.get_usage_example_if_representation_could_be_found() is None
+
+    def test_get_usage_example_if_representation_could_be_found_too_long(self):
+        record = Record(
+            id="1",
+            text=" skrutits skrutits skrutits skrutits skrutits skrutits skrutits "
+            "skrutits skrutits skrutits skrutits skrutits skrutits skrutits skrutits skrutits ",
+            language_code=WikimediaLanguageCode.SWEDISH,
+            language_style=LanguageStyle.INFORMAL,
+            source=SupportedExampleSources.HISTORICAL_ADS,
+            type_of_reference=ReferenceType.WRITTEN,
+            form=self.example_form,
+        )
+        assert record.get_usage_example_if_representation_could_be_found() is None
+
+    def test_get_usage_example_if_representation_could_be_found_valid(self):
+        record = Record(
+            id="1",
+            text=" skrutits skrutits skrutits skrutits "
+            "skrutits skrutits skrutits skrutits skrutits skrutits skrutits skrutits skrutits ",
+            language_code=WikimediaLanguageCode.SWEDISH,
+            language_style=LanguageStyle.INFORMAL,
+            source=SupportedExampleSources.HISTORICAL_ADS,
+            type_of_reference=ReferenceType.WRITTEN,
+            form=self.example_form,
+        )
+        ue = record.get_usage_example_if_representation_could_be_found()
+        assert isinstance(ue, UsageExample)
+        assert ue.text == (
+            " skrutits skrutits skrutits skrutits skrutits skrutits skrutits skrutits "
+            "skrutits skrutits skrutits skrutits skrutits "
+        )
+
+    # def test_lookup_qid(self):
+    #     assert False
+    #
+    def test_url_no_baseurl(self):
+        record = Record(
+            id="1",
+            text=" skrutits skrutits skrutits skrutits "
+            "skrutits skrutits skrutits skrutits skrutits skrutits skrutits skrutits skrutits ",
+            language_code=WikimediaLanguageCode.SWEDISH,
+            language_style=LanguageStyle.INFORMAL,
+            source=SupportedExampleSources.HISTORICAL_ADS,
+            type_of_reference=ReferenceType.WRITTEN,
+            form=self.example_form,
+        )
+        with self.assertRaises(ValueError):
+            assert record.url == ""
+
+    def test_url_with_baseurl(self):
+        record = Record(
+            base_url="test",
+            id="1",
+            text=" skrutits skrutits skrutits skrutits "
+            "skrutits skrutits skrutits skrutits skrutits skrutits skrutits skrutits skrutits ",
+            language_code=WikimediaLanguageCode.SWEDISH,
+            language_style=LanguageStyle.INFORMAL,
+            source=SupportedExampleSources.HISTORICAL_ADS,
+            type_of_reference=ReferenceType.WRITTEN,
+            form=self.example_form,
+        )
+        assert record.url == 'test1'
+
+    #
+    # def test_human_readable_url(self):
+    #     assert False
